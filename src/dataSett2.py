aab9d24907f1862c6db86cd6dfbf92ad9bb631b0
evinef <evinehf@gmail.com>
2025-03-25 12:18:50 +0100

    datasett2
    lagt inn datasett 2

---
+++ src/dataSett2.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from meteostat import Stations, Daily
+import pandas as pd
+
+# Set time period
+start = datetime(2013, 3, 1)
+end = datetime(2023, 3, 1)
+
+# Get daily data
+data = Daily('72509', start, end)
+data = data.fetch()
+data=data.reset_index().iloc[:,[0,1,2,3,4,6,7,9]]
+
+data.to_csv('bostonData2.csv',index=False)
